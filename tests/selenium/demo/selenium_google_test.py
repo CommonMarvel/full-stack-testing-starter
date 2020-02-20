0513bf13130a1a7067969c2295f41cc98ea044f3
Huang Jian-Min <yfr.huang@hotmail.com>
2020-02-20 22:21:06 +0800

    feat: Add first demo selenium test

--- tests/selenium/demo/selenium_google_test.py
+++ tests/selenium/demo/selenium_google_test.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
@@ -5,24 +7,25 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 
 @pytest.mark.parametrize("env", ["dev"])
-def test_selenium_login(setup_function, env):
+def test_selenium_google(setup_function, env):
     config = setup_function
-    timeout = config["demo"][env]["timeout"]
-
-    options = webdriver.FirefoxOptions()
+    timeout = config["demo"][env]["selenium"]["timeout"]
 
     executable_path = "./drivers/geckodriver_mac_greater_60"
 
+    options = webdriver.FirefoxOptions()
     options.headless = True
     options.add_argument("--disable-gpu")
 
     browser = webdriver.Firefox(executable_path=executable_path, options=options)
-    browser.implicitly_wait(300)
-    browser.get("http://www.google.com.tw")
+    browser.implicitly_wait(timeout)
+    browser.get(config["demo"][env]["selenium"]["host"])
 
     WebDriverWait(browser, timeout, 2).until(EC.visibility_of(browser.find_element_by_css_selector("input[name='q']")))
     search_txt = browser.find_element_by_css_selector("input[name='q']")
+    sleep(1)
     search_txt.send_keys("selenium")
+    sleep(1)
     search_txt.submit()
 
     WebDriverWait(browser, timeout, 2).until(EC.visibility_of(browser.find_element_by_css_selector("input[name='q']")))
