42da01112e1c2e65d44e449f2a218bb20ab6de8b
Huang Jian-Min <yfr.huang@hotmail.com>
2020-03-20 23:07:45 +0800

    update image

--- tests/selenium/demo/selenium_google_test.py
+++ tests/selenium/demo/selenium_google_test.py
@@ -5,12 +5,14 @@ from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
-
+# Test 3A (arrange, act, assert)
 @pytest.mark.parametrize("env", ["dev"])
 def test_selenium_google(setup_function, env):
+    # get config from setup function (arrange)
     config = setup_function
     timeout = config["demo"][env]["selenium"]["timeout"]
 
+    # setting browser (arrange)
     executable_path = "./drivers/geckodriver_mac_greater_60"
 
     options = webdriver.FirefoxOptions()
@@ -19,6 +21,8 @@ def test_selenium_google(setup_function, env):
 
     browser = webdriver.Firefox(executable_path=executable_path, options=options)
     browser.implicitly_wait(timeout)
+
+    # operate browser (act)
     browser.get(config["demo"][env]["selenium"]["host"])
 
     WebDriverWait(browser, timeout, 2).until(EC.visibility_of(browser.find_element_by_css_selector("input[name='q']")))
@@ -28,8 +32,11 @@ def test_selenium_google(setup_function, env):
     sleep(1)
     search_txt.submit()
 
+    # get result (act)
     WebDriverWait(browser, timeout, 2).until(EC.visibility_of(browser.find_element_by_css_selector("input[name='q']")))
     search_txt = browser.find_element_by_css_selector("input[name='q']")
+
+    # assert
     assert search_txt.get_attribute("value") == "selenium"
 
     browser.quit()
